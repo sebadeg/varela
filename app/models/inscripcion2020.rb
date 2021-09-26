5795c358855bd1aaccba3197d9b7758651928369
sebadeg <sebastian.degrandi@gmail.com>
2021-09-26 08:32:17 -0300

    Update inscripcion2020.rb

--- app/models/inscripcion2020.rb
+++ app/models/inscripcion2020.rb
@@ -34,7 +34,7 @@ class Inscripcion2020 < ApplicationRecord
 
   def self.OpcionesGrados(inscripcionAlumno)
     opciones = Array.new
-    ProximoGrado.where("grado_id=#{inscripcionAlumno.grado_id} AND anio=2021").order(:nombre).each do |opcion|
+    ProximoGrado.where("grado_id=#{inscripcionAlumno.grado_id} AND anio=2022").order(:nombre).each do |opcion|
       opciones.push( ["#{opcion.nombre} - $#{opcion.precio.to_i.to_s}",opcion.id] )
     end 
     return opciones
